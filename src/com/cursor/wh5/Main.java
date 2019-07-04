8e297997662578a6f06bd905f9f18e9e2a31241c
falcon89 <vasyl.kachala@gmail.com>
2019-07-04 12:06:49 +0300

    Remove

--- src/com/cursor/wh5/Main.java
+++
@@ -1,9 +0,0 @@
-package com.cursor.wh5;
-
-import static com.cursor.wh5.LibraryService.libraryCase;
-
-public class Main {
-    public static void main(String[] args) {
-        libraryCase();
-    }
-}
